88ccb46c83b3597f1c21c105d743b9b61d91914b
slenksp <slenksp@github.com>
2019-11-25 21:08:31 -0600

    added supervisor

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -21,7 +21,16 @@ let doSupervisorActivity = () => {
         choices: ["View Product Sales By Department", "Create New Department"],
         default: "View Product Sales By Department"
     }).then((answer) => {
-        answer.activity === "View Product Sales By Department" ? viewProductSalesByDepartment() : createNewDepartMent();
+        switch (answer.activity) {
+            case "View Product Sales By Department":
+                viewProductSalesByDepartment();
+                break;
+            case "Create New Department":
+                createNewDepartment();
+                break;
+            default:
+                exitSection();
+        }
     });
 }
 let viewProductSalesByDepartment = () => {
@@ -29,9 +38,42 @@ let viewProductSalesByDepartment = () => {
     totalSalesByDepartment();
 
 }
+let department_id = [];
+connection.query(`SELECT department_id FROM bamazon_db.departments`, (err, res) => {
+    if (err) throw err;
+    res.map(function (item) {
+        department_id.push(item.department_id);
+    });
+})
 let createNewDepartment = () => {
     console.log(`\nAdd New Department: \n---------------------\n`);
-    doAnotherActivity();
+    inquirer.prompt({
+        type: "number",
+        name: "dept_id",
+        message: "Enter the department number (000-999)"
+    }).then((ans) => {
+        if (department_id.includes(ans.dept_id)) {
+            console.log(`\nDepartment ID already exists! Try another ID`);
+            createNewDepartment();
+        } else {
+            inquirer.prompt([{
+                type: "text",
+                name: "dept_name",
+                message: "Enter the department name"
+            }, {
+                type: "number",
+                name: "over_head_cost",
+                message: "Enter the over head cost"
+            }]).then((answer) => {
+                console.log(`You entered: \n-----------\n
+                Department ID: ${ans.dept_id}\n
+                Department Name: ${answer.dept_name}\n
+                Over Head Cost: $${answer.over_head_cost}\n`);
+                addDept(ans.dept_id, answer.dept_name, answer.over_head_cost);
+                setTimeout(doAnotherActivity, 1000);
+            });
+        }
+    });
 }
 
 let doAnotherActivity = () => {
@@ -44,12 +86,20 @@ let exitSection = () => {
     process.exit();
 }
 let totalSalesByDepartment = () => {
-    connection.query(`SELECT department_name,
-                    SUM(product_sales)
-                    FROM products
-                    GROUP BY department_name;`, (err, res) => {
+    connection.query(`SELECT departments.*, SUM(products.product_sales) as sales, 
+SUM(products.product_sales)-departments.over_head_cost as total_profit
+FROM products
+RIGHT JOIN departments ON products.department_name= departments.department_name
+GROUP BY departments.department_id, departments.department_name, departments.over_head_cost;`, (err, res) => {
         if (err) throw err;
         console.table(res);
     });
-    setTimeout(doAnotherActivity,1000);
+    setTimeout(doAnotherActivity, 1000);
+}
+let addDept = (id, name, cost) => {
+    connection.query(`INSERT INTO departments(department_id,department_name, over_head_cost)
+    VALUES(${id },"${ name }",${ cost })`, (err, res) => {
+        if (err) throw err;
+        console.log(`Department Added Successfully!`);
+    });
 }
